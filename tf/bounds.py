77708c4b9d1a7508f02bb391c45b7657df261d35
Gordon Stewart <gstew5@gmail.com>
2018-04-09 14:33:20 -0400

    Add a script to compute generalization bounds

---
+++ tf/bounds.py
@@ -0,0 +1,20 @@
+from math import *
+
+# Compute Chernoff-style generalization bounds: 
+#   ln_size_h = ln(size_hypothesis_class)
+#   m = number of training samples
+#   delta = confidence
+def bounds(ln_size_h, m, delta=0.01):
+    return \
+        sqrt(   (log(2) + ln_size_h + log(1 / delta)) / (2.0*m)   )
+
+def ln_size_h(inputs=784, outputs=10, hidden_nodes=10, bitwidth=16):
+    d = inputs*hidden_nodes + hidden_nodes*outputs
+    return bitwidth*d / log(2, e)
+
+def values():
+    for m in range(100000, 1000000, 50000): 
+        print('m=%d, error_bound=%f' % (m, bounds(ln_size_h(), m)))
+
+values()        
+    
