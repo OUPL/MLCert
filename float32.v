2039a75673e2af232413aa67d88ed06148fe118f
Gordon Stewart <gstew5@gmail.com>
2018-07-03 17:29:04 -0400

    Proved-generalizing Perceptron

---
+++ float32.v
@@ -0,0 +1,63 @@
+Set Implicit Arguments.
+Unset Strict Implicit.
+
+Require Import mathcomp.ssreflect.ssreflect.
+From mathcomp Require Import all_ssreflect.
+
+Require Import List. Import ListNotations.
+Require Import Extraction.
+
+(*Axiomatized basic types, together with cardinality proofs*)
+Axiom float32 : Type. (*32-bit floating-point numbers*)
+Axiom float32_finite : Finite.class_of float32.
+Definition float32_finType : finType := Finite.Pack float32_finite float32.
+Axiom float32_card : #|float32_finType| = 2^32.
+Axiom float32_arr : forall n:nat, Type. (*size-indexed float32 arrays*)
+Axiom float32_arr_finite : forall n:nat, Finite.class_of (float32_arr n).
+Definition float32_arr_finType (n:nat) : finType :=
+  Finite.Pack (float32_arr_finite n) (float32_arr n).
+Axiom float32_arr_card : forall n, #|float32_arr_finType n| = 2^(n*32).
+
+Extract Constant float32 => "Prelude.Float".
+Extract Constant float32_arr => "[Prelude.Float]".
+Extract Inductive bool => "Prelude.Bool" ["Prelude.True" "Prelude.False"].
+
+(*Axiomatized arithmetic expressions*)
+Axiom f32_0 : float32.
+Axiom f32_1 : float32.
+Axiom f32_opp : float32 -> float32.
+Axiom f32_gt : float32 -> float32 -> bool.
+Axiom f32_add : float32 -> float32 -> float32.
+Axiom f32_mult : float32 -> float32 -> float32.
+Axiom f32_init : forall (n:nat)(init:float32), float32_arr n.
+Axiom f32_map : forall (n:nat)(f:float32->float32)(a:float32_arr n), float32_arr n.
+Axiom f32_map2 : forall (n:nat)(f:float32->float32->float32)(a b:float32_arr n), float32_arr n.
+Axiom f32_fold2 : forall (T:Type)(n:nat)(t0:T)(f:float32->float32->T->T)(a1 a2:float32_arr n), T.
+
+Extract Constant f32_0 => "(0.0 :: Prelude.Float)".
+Extract Constant f32_1 => "(1.0 :: Prelude.Float)".
+Extract Constant f32_opp => "(\f -> - f)".
+Extract Constant f32_gt => "(\f1 f2 -> (Prelude.>) f1 f2)".
+Extract Constant f32_add => "(\f1 f2 -> (Prelude.+) f1 f2)".
+Extract Constant f32_mult => "(\f1 f2 -> (Prelude.*) f1 f2)".
+Extract Constant f32_init => "(\n init -> Prelude.take n (Prelude.repeat init))".
+Extract Constant f32_map => "(\_ f a -> Prelude.map f a)".
+Extract Constant f32_map2 => "(\_ f a1 a2 -> Prelude.map (\(x,y) -> f x y) (Prelude.zip a1 a2))".
+Extract Constant f32_fold2 => "(\_ t f a1 a2 -> Prelude.foldl (\acc (x, y) -> f x y acc) t (Prelude.zip a1 a2))".
+
+Notation "0" := (f32_0) : f32_scope.
+Notation "1" := (f32_1) : f32_scope.
+Infix ">" := (f32_gt) : f32_scope.
+Infix "+" := (f32_add) : f32_scope. 
+Infix "*" := (f32_mult) : f32_scope.
+
+Definition neg1 : float32 := f32_opp f32_1.
+Definition float32_of_bool (b:bool) := if b then f32_1 else neg1.
+Coercion float32_of_bool : bool >-> float32.
+
+Section f32_definitions.
+  Open Scope f32_scope.
+  
+  Definition f32_dot (n:nat) (a1 a2:float32_arr n) : float32 :=
+    f32_fold2 0 (fun x1 x2 sum => (x1 * x2) + sum) a1 a2.
+End f32_definitions.
---
+++ linearclassifiers.v
@@ -0,0 +1,117 @@
+Set Implicit Arguments.
+Unset Strict Implicit.
+
+Require Import mathcomp.ssreflect.ssreflect.
+From mathcomp Require Import all_ssreflect.
+
+Require Import List. Import ListNotations.
+Require Import Extraction.
+
+Require Import float32.
+
+Section LinearClassifier.
+  Variable n : nat. (*the dimensionality*)
+
+  Definition A := float32_arr n. (*examples*)
+  Definition B := bool. (*labels*)
+  Definition Weights := float32_arr n.
+  Definition Bias := float32.
+  Definition Params := (Weights * Bias)%type.
+
+  Section predict.
+    Open Scope f32_scope.
+    Definition predict (p : Params) (a : A) : B :=
+      let: (w, b) := p in
+      f32_dot w a + b > 0.
+  End predict.    
+End LinearClassifier.
+
+Module Learner.
+  Record t (A B hypers params : Type) :=
+    mk { predict : hypers -> params -> A -> B;
+         update : hypers -> A*B -> params -> params }.
+End Learner.
+
+Section learning.
+  Variables A B hypers params : Type.
+  Variable learner : Learner.t A B hypers params.
+  Variable h : hypers.
+
+  Fixpoint learn (p:params) (training_set:list (A*B)) : params :=
+    match training_set with
+    | [] => p
+    | ab :: training_set' => learn (Learner.update learner h ab p) training_set'
+    end.
+End learning.
+
+Module Perceptron.
+  Section Learner.
+    Variable n : nat.     
+    Notation A := (A n).
+    Notation B := B.
+    Notation Params := (Params n).
+
+    Record Hypers : Type :=
+      mkHypers { alpha : float32 }.
+
+    Open Scope f32_scope.    
+    
+    Definition update (h:Hypers) (example_label:A*B) (p:Params) : Params :=
+      let: (example, label) := example_label in
+      let: predicted_label := predict p example in
+      if Bool.eqb predicted_label label then p
+      else let: (w, b) := p in
+           (f32_map2 (fun x1 x2 => x1 + (alpha h)*label*x2) w example, b+label).
+    
+    Definition Learner : Learner.t A B Hypers Params :=
+      Learner.mk
+        (fun _ => @predict n)
+        update.
+  End Learner.
+End Perceptron.
+
+Module PerceptronExtractionTest.
+  Local Open Scope f32_scope.
+  Definition n := 2.
+  Definition alpha : Perceptron.Hypers := Perceptron.mkHypers 1.
+  Definition training_set ex1 ex2 : list (A n*B) := [(ex1,true); (ex2,false)].
+  Definition go init_weights ex1 ex2 :=
+    @learn
+      _ _ _ _
+      (Perceptron.Learner n) alpha (init_weights, 0) (training_set ex1 ex2).
+  Extraction Language Haskell.
+  Extraction "Perceptron.hs" go.
+End PerceptronExtractionTest.
+
+Require Import Reals Fourier.
+Require Import OUVerT.bigops OUVerT.dist OUVerT.chernoff OUVerT.learning.
+
+Section PerceptronGeneralization.
+  Variable n : nat. (*The dimensionality*)
+  Notation A := (float32_arr_finType n).
+  Notation B := bool_finType.
+  Variable d : A * B -> R.
+  Variable d_dist : big_sum (enum [finType of A * B]) d = 1.
+  Variable d_nonneg : forall x, 0 <= d x.
+
+  Variable m : nat. (*The number of training samples*)
+  Variable m_gt0 : (0 < m)%nat.
+
+  Variable h : Perceptron.Hypers.
+
+  (*J is 0-1 loss applied to Perceptron's prediction function*)
+  Notation Params := [finType of A * float32_finType].
+  Definition J := @loss01 A _ m Params (Learner.predict (Perceptron.Learner n) h).
+  
+  Lemma chernoff_bound_loss01_perceptron
+      (eps : R) (eps_gt0 : 0 < eps)
+      (not_perfectly_learnable : forall p : Params, 0 < expErr d m_gt0 J p < 1)
+      (ind : forall p : Params, mutual_independence d (J p)) :
+    probOfR (prodR (fun _ : 'I_m => d))
+          [pred T:training_set A B m
+          | [exists i : 'I_#|eps_Hyp d m_gt0 J eps|,
+             let: h := projT1 (enum_val i)
+             in Rle_lt_dec eps (Rabs (expErr d m_gt0 J h - empErr J T h))]]
+    <= 2 * INR #|Params| * exp (-2%R * eps^2 * mR m).
+  Proof. by apply: chernoff_bound_loss01. Qed.
+End PerceptronGeneralization.
