c188db6c3fce4d1bb7489b852dbdf25ae0ff9baa
Gordon Stewart <gstew5@gmail.com>
2018-07-11 18:59:21 -0400

    Report generalization error in PerceptronTest.hs

--- hs/PerceptronTest.hs
+++ hs/PerceptronTest.hs
@@ -14,9 +14,9 @@ fromInt n | n > 0 = S (fromInt $ n - 1)
 fromNat O = 0
 fromNat (S n) = 1 + fromNat n
 
-n = fromInt 100
-m = fromInt 1000
-epochs = fromInt 10
+n = fromInt 2 --the number of dimensions
+m = fromInt 7500 --the number of samples
+epochs = fromInt 5
 
 hypers = MkHypers 1.0 (fromIntegral (fromNat m) / 2.0)
 
@@ -28,39 +28,54 @@ init_weights = take (fromNat n) $ repeat 0.0
 init_bias :: Bias
 init_bias = 0.0
 
-sampler _ f = training_set n m >>= f
+sampler hyperplane _ f = training_set hyperplane n m >>= f
 
 training_example O = return []
 training_example (S n) =
   do { r <- randomRIO (-1.0,1.0)
      ; e <- training_example n
      ; return $ r : e }
-training_row n = 
+training_row hyperplane n = 
   do { example <- training_example n
-     ; label <- randomRIO (0,1)
-     ; return $ Pair example (int2bool label) }
+     ; let label = predict n 0.0 (Pair hyperplane init_bias) example
+     ; return $ Pair example label }
   where int2bool :: Int -> Bool
         int2bool 0 = False
         int2bool 1 = True
 
-training_set _ O = return []
-training_set n (S m)
-  = do { r <- training_row n
-       ; t <- training_set n m
+training_set _ _ O = return []
+training_set hyperplane n (S m)
+  = do { r <- training_row hyperplane n
+       ; t <- training_set hyperplane n m
        ; return $ r : t }
+
+test_set = training_set
   
-show_test_err (Pair model examples_labels) =
-  let corrects = 
+print_generalization_err test (Pair model training) =
+  let corrects dataset = 
         map (\(Pair example label) ->
                 if predict n (theta hypers) model example == label
                 then 1 :: Int
-                else 0) examples_labels
+                else 0) dataset
+      percent_correct_training
+        = fromIntegral (sum $ corrects training) / fromIntegral (fromNat m)
+      percent_correct_test
+        = fromIntegral (sum $ corrects test) / fromIntegral (fromNat m)
   in putStrLn
-     $ show
-     $ fromIntegral (sum corrects) / fromIntegral (fromNat m)
+     $ "Training: " ++ show percent_correct_training ++ "\n"
+        ++ "Test: " ++ show percent_correct_test ++ "\n"
+        ++ "Generalization Error: "
+        ++ show (percent_correct_training - percent_correct_test)
 
 main =
-  perceptron n m epochs hypers sampler dist
-    (Pair init_weights init_bias) show_test_err
+  do { hyperplane <- training_example n
+     ; test <- test_set hyperplane n m 
+     ; perceptron n m epochs hypers (sampler hyperplane) dist
+         (Pair init_weights init_bias) (print_generalization_err test) }
 
-
+-- NOTES:
+-- inputs:
+-- n = 2
+-- m = 7500
+-- eps = 0.05
+-- should yield probability: 4.45e-7
