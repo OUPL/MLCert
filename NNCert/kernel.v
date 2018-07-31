01281698dd88e1a10782cc0cbbe6f262d275e02d
Gordon Stewart <gstew5@gmail.com>
2018-07-31 16:45:57 -0400

    More work on cardinality proofs

--- NNCert/kernel.v
+++ NNCert/kernel.v
@@ -19,11 +19,13 @@ Module Type TYPE.
   Parameter t : Type.
 End TYPE.  
 
-
 Module Type FINTYPE.
   Parameter t : finType.
-End FINTYPE.  
+End FINTYPE.
 
+Module TypeOfFintype (T:FINTYPE) <: TYPE.
+  Definition t:Type := T.t.
+End TypeOfFintype.
 
 (** We could use flattened vectors instead of matrices I guess but
     compilation and extraction seem to be a bit faster this way. *)
@@ -65,6 +67,7 @@ Module Kernel (IN N OUT : BOUND) (S T : TYPE) <: KernelType IN N OUT S T.
       layer2 : Layer2 }.
 End Kernel.
 
+
 (** A finType analog of KernelType:*)
 Module Type KernelFinType (IN N OUT : BOUND) (S T : FINTYPE).
   Definition Layer1Payload := AxVec_finType IN.n T.t.
@@ -81,6 +84,7 @@ Module Type KernelFinType (IN N OUT : BOUND) (S T : FINTYPE).
   Definition l1 (x:t) := let: (ss1, ss2, l1, l2) := x in l1.
   Definition l2 (x:t) := let: (ss1, ss2, l1, l2) := x in l2.  
 End KernelFinType.
+
 
 Module KernelFin (IN N OUT : BOUND) (S T : FINTYPE) <: KernelFinType IN N OUT S T.
   Definition Layer1Payload := AxVec_finType IN.n T.t.
@@ -217,6 +221,8 @@ Import DyadicFloat16. (*for bits_to_bvec*)
 Definition bitvec_to_bvec (n:nat) (v:bitvec n) : BitVec.t :=
   bits_to_bvec (bitvec_to_sparse_list v).
 
+(** Cardinality proof for b=16, N=10*)
+
 Module bitvec16Type <: TYPE.
   Definition t := bitvec 16.
 End bitvec16Type.
@@ -231,36 +237,101 @@ Module bitvec16PayloadMap : PayloadMap bitvec16Type.
 End bitvec16PayloadMap.
 
 Module IN_784 <: BOUND. Definition n := 784. Lemma n_gt0 : 0 < n. by []. Qed. End IN_784.
-Module N_20 <: BOUND. Definition n := 20. Lemma n_gt0 : 0 < n. by []. Qed. End N_20.
+Module N_10 <: BOUND. Definition n := 10. Lemma n_gt0 : 0 < n. by []. Qed. End N_10.
 Module OUT_10 <: BOUND. Definition n := 10. Lemma n_gt0 : 0 < n. by []. Qed. End OUT_10.
 
-Module bitvec16_EMNIST_20_KernelType 
-  : KernelType IN_784 N_20 OUT_10 bitvec16Type bitvec16Type
-  := Kernel IN_784 N_20 OUT_10 bitvec16Type bitvec16Type.
+Module bitvec16_EMNIST_10_KernelType 
+  : KernelType IN_784 N_10 OUT_10 bitvec16Type bitvec16Type
+  := Kernel IN_784 N_10 OUT_10 bitvec16Type bitvec16Type.
 
-Module bitvec16_EMNIST_20_KernelFinType 
-  : KernelFinType IN_784 N_20 OUT_10 bitvec16FinType bitvec16FinType
-  := KernelFin IN_784 N_20 OUT_10 bitvec16FinType bitvec16FinType.
+Module bitvec16_EMNIST_10_KernelFinType 
+  : KernelFinType IN_784 N_10 OUT_10 bitvec16FinType bitvec16FinType
+  := KernelFin IN_784 N_10 OUT_10 bitvec16FinType bitvec16FinType.
 
-Lemma card_bitvec16_EMNIST_20_KernelFinType :
-  #|bitvec16_EMNIST_20_KernelFinType.t|
-  = 2^(4*16 + 20*784*16 + 10*20*16). (*2^254144 causes stack overflow*)
+Lemma card_bitvec16_EMNIST_10_KernelFinType :
+  #|bitvec16_EMNIST_10_KernelFinType.t|
+  = 2^(4*16 + 10*784*16 + 10*10*16). (*2^254144 causes stack overflow*)
 Proof.
-  rewrite /bitvec16_EMNIST_20_KernelFinType.t !card_prod bitvec16FinType.card.
+  rewrite /bitvec16_EMNIST_10_KernelFinType.t !card_prod bitvec16FinType.card.
   (*Layer 1*)
-  rewrite /bitvec16_EMNIST_20_KernelFinType.Layer1.
-  rewrite /bitvec16_EMNIST_20_KernelFinType.Layer1Payload.
+  rewrite /bitvec16_EMNIST_10_KernelFinType.Layer1.
+  rewrite /bitvec16_EMNIST_10_KernelFinType.Layer1Payload.
   have H1: #|AxVec_finType IN_784.n bitvec16FinType.t| = 2^(784*16).
   { rewrite (@AxVec_card 784 16) => //.
     by rewrite bitvec_card. }
-  rewrite (@AxVec_card 20 (784*16) _ H1).
+  rewrite (@AxVec_card 10 (784*16) _ H1).
   (*Layer 2*)
-  rewrite /bitvec16_EMNIST_20_KernelFinType.Layer2.
-  rewrite /bitvec16_EMNIST_20_KernelFinType.Layer2Payload.
-  have H2: #|AxVec_finType N_20.n bitvec16FinType.t| = 2^(20*16).
-  { rewrite (@AxVec_card 20 16 bitvec16FinType.t); first by reflexivity.
+  rewrite /bitvec16_EMNIST_10_KernelFinType.Layer2.
+  rewrite /bitvec16_EMNIST_10_KernelFinType.Layer2Payload.
+  have H2: #|AxVec_finType N_10.n bitvec16FinType.t| = 2^(10*16).
+  { rewrite (@AxVec_card 10 16 bitvec16FinType.t); first by reflexivity.
     rewrite bitvec_card; reflexivity. }
-  rewrite (@AxVec_card 10 (20*16) _ H2).
+  rewrite (@AxVec_card 10 (10*16) _ H2).
   rewrite -!multE; rewrite <-!Nat.pow_add_r; rewrite !multE; reflexivity.
 Qed.  
-  
+
+(** Cardinality proof for b=16, N=10*)
+
+Module bitvec2Type <: TYPE.
+  Definition t := bitvec 2.
+End bitvec2Type.
+
+Module bitvec2FinType <: FINTYPE.
+  Definition t := bitvec_finType 2.
+  Lemma card : #|t| = 2^2. Proof. by rewrite bitvec_card. Qed.
+End bitvec2FinType.
+
+Module bitvec2PayloadMap : PayloadMap bitvec2Type.
+  Definition f (v:bitvec2Type.t) : DRed.t := to_dyadic (bitvec_to_bvec v).
+End bitvec2PayloadMap.
+
+Module bitvec2_EMNIST_10_KernelType 
+  : KernelType IN_784 N_10 OUT_10 bitvec16Type bitvec2Type
+  := Kernel IN_784 N_10 OUT_10 bitvec16Type bitvec2Type.
+
+Module bitvec2_EMNIST_10_KernelFinType 
+  : KernelFinType IN_784 N_10 OUT_10 bitvec16FinType bitvec2FinType
+  := KernelFin IN_784 N_10 OUT_10 bitvec16FinType bitvec2FinType.
+
+Lemma card_bitvec2_EMNIST_10_KernelFinType :
+  #|bitvec2_EMNIST_10_KernelFinType.t|
+  = 2^(4*16 + 10*784*2 + 10*10*2). 
+Proof.
+  rewrite /bitvec2_EMNIST_10_KernelFinType.t !card_prod bitvec16FinType.card.
+  (*Layer 1*)
+  rewrite /bitvec2_EMNIST_10_KernelFinType.Layer1.
+  rewrite /bitvec2_EMNIST_10_KernelFinType.Layer1Payload.
+  have H1: #|AxVec_finType IN_784.n bitvec2FinType.t| = 2^(784*2).
+  { rewrite (@AxVec_card 784 2) => //.
+    by rewrite bitvec_card. }
+  rewrite (@AxVec_card 10 (784*2) _ H1).
+  (*Layer 2*)
+  rewrite /bitvec2_EMNIST_10_KernelFinType.Layer2.
+  rewrite /bitvec2_EMNIST_10_KernelFinType.Layer2Payload.
+  have H2: #|AxVec_finType N_10.n bitvec2FinType.t| = 2^(10*2).
+  { rewrite (@AxVec_card 10 2 bitvec2FinType.t); first by reflexivity.
+    rewrite bitvec_card; reflexivity. }
+  rewrite (@AxVec_card 10 (10*2) _ H2).
+  rewrite -!multE; rewrite <-!Nat.pow_add_r; rewrite !multE; reflexivity.
+Qed.  
+
+(*
+Module b16 := TypeOfFintype bitvec16FinType.
+Module b2 := TypeOfFintype bitvec2FinType.
+Module KTranslate :=
+  Translate IN_784 N_10 OUT_10 b16 b2 bitvec16PayloadMap bitvec2PayloadMap
+            bitvec2_EMNIST_10_KernelType.
+Import KTranslate. Import TheNet.
+Import F. Import NETEval. Import NET.
+
+Require Import Reals Fourier.
+Require Import OUVerT.bigops OUVerT.dist OUVerT.chernoff OUVerT.learning.
+
+Require Import MLCert.oracleclassifiers.
+
+Definition X := NETEval.InputEnv.t.
+Definition Y := Output.t.
+
+Definition Params := bitvec2_EMNIST_10_KernelType.t.
+*)
+
