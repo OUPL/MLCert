1c24b1177f89d2371d29846c601792e345ac098e
Alexander Bagnall <ab667712@ohio.edu>
2018-09-03 17:58:26 -0400

    Remove a couple more files. I think these are old.

--- NNCert/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set num_batches in empiricalloss.v.
-
-python3 compile.py params.pkl.gz && time make && \
-    time coqc empiricalloss.v -R . Top && cd extract && \
-    rm batch_test.mli && time ocamlc batch_test.ml && time ./a.out
