bc7a1ce099d67c3c211f602507e539dd7db114f0
Gordon Stewart <gstew5@gmail.com>
2018-11-13 16:25:05 -0500

    Hacking on KPerceptronTest with Robin

--- hs/KPerceptronTest.hs
+++ hs/KPerceptronTest.hs
@@ -12,10 +12,10 @@ fromInt n | n > 0 = S (fromInt $ n - 1)
 fromNat O = 0
 fromNat (S n) = 1 + fromNat n
 
-n = fromInt 3 --the number of dimensions
+n = fromInt 20 --the number of dimensions
 --m = fromInt 7500 --the number of samples
 -- The following m is better for plotting:
-m = fromInt 20 -- 7500 --the number of samples
+m = fromInt 2000 -- 7500 --the number of samples
 epochs = fromInt 5
 
 dist _ = -1.0 --not used in sampler below
@@ -24,45 +24,44 @@ init_weights :: KernelParams
 init_weights = take (fromNat m) $ repeat 0.0
 
 print_training_set [] = return ()
-print_training_set ((xs,y) : t) =
+print_training_set (((i,xs),y) : t) =
   let print_xs [] = return ()
       print_xs (x : xs) = putStr (show x) >> putStr "," >> print_xs xs
   in
-  do { print_xs xs
+  do { putStrLn (show $ fromNat i)
+     ; print_xs xs
      ; putStrLn (show y)
      ; print_training_set t }
 
-sampler hyperplane _ f =
-  do { t <- training_set hyperplane n m
-     ; putStrLn "Training Set:"
-     ; print_training_set t
-     ; f t }
+sampler data_set _ f =
+  do { putStrLn "Training Set:"
+     ; print_training_set data_set
+     ; f data_set }
+
+kernel_predict_specialized examples hyperplane ak =
+  kernel_predict n m list_Foldable examples hyperplane ak
 
 training_example O = return []
 training_example (S n) =
   do { r <- randomRIO (-1.0,1.0)
      ; e <- training_example n
      ; return $ r : e }
-training_row hyperplane n = 
+training_row i n = 
   do { example <- training_example n
-     ; let label = kernel_predict n hyperplane (n,example)
-     ; return (example, label) }
-  where int2bool :: Int -> Bool
-        int2bool 0 = False
-        int2bool 1 = True
+     ; label <- randomRIO (False,True)
+     ; return ((i, example), label) }
 
-training_set _ _ O = return []
-training_set hyperplane n (S m)
-  = do { r <- training_row hyperplane n
-       ; t <- training_set hyperplane n m
+data_set :: Nat -> Nat -> IO [(Ak, Bk)]
+data_set _ O = return []
+data_set n (S m)
+  = do { r <- training_row m n
+       ; t <- data_set n m
        ; return $ r : t }
-
-test_set = training_set
 
 print_generalization_err test (model, training) =
   let corrects dataset = 
         map (\(example, label) ->
-                if kernel_predict n model example == label
+                if kernel_predict_specialized training model example == label
                 then 1 :: Int
                 else 0) dataset
       percent_correct_training
@@ -74,13 +73,20 @@ print_generalization_err test (model, training) =
        ++ "Training: " ++ show percent_correct_training ++ "\n"
        ++ "Test: " ++ show percent_correct_test ++ "\n"
        ++ "Generalization Error: "
-       ++ show (percent_correct_training - percent_correct_test)
+       ++ show (abs (percent_correct_training - percent_correct_test))
 
 main =
+  do { test <- data_set n m
+     ; train <- data_set n m 
+     ; kperceptron n m epochs list_Foldable train (sampler train) dist
+         init_weights (print_generalization_err test) }
+
+
+{-main =
   do { hyperplane <- training_example n
      ; test <- test_set hyperplane n m 
-     ; kperceptron n epochs O (sampler hyperplane) dist
-         init_weights (print_generalization_err test) }
+     ; kperceptron n m epochs list_Foldable (sampler hyperplane) dist
+         init_weights (print_generalization_err test) }-}
 
 -- NOTES:
 -- inputs:
