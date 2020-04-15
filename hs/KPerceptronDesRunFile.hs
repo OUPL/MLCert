862cc419afac8e01fbeac9dca9bf8f73b941cb59
robinjanette <rk345613@ohio.edu>
2020-04-15 14:33:24 -0400

    Adding description driver for running dataset files

--- hs/KPerceptronDesRunFile.hs
+++ hs/KPerceptronDesRunFile.hs
@@ -3,6 +3,9 @@
 module Main where
 
 import System.Random
+import System.IO
+import Data.List.Split
+import Data.Bool
 
 import KPerceptronDes
 
@@ -13,8 +16,8 @@ fromNat O = 0
 fromNat (S n) = 1 + fromNat n
 
 n = fromInt 3 --the number of dimensions
-m = fromInt 200 --the number of samples
-des = fromInt 19 --the number of support vectors
+m = fromInt 1000 --the number of samples
+des = fromInt 99 --the number of support vectors
 epochs = fromInt 5
 
 dist _ = -1.0 --not used in sampler below
@@ -27,10 +30,10 @@ makeDesParamhelper (S des) = (init_weights, False) : (makeDesParamhelper des)
 
 makeDesParams dataset = (0.0, makeDesParamhelper (S des))
 
-categorize :: Nat -> (,) Float32_arr Float32 -> [Float32] -> Bkd
+{-categorize :: Nat -> (,) Float32_arr Float32 -> [Float32] -> Bkd
 categorize n p a =
   case p of {
-   (,) w b -> f32_gt (f32_add (f32_dot n w a) b) f32_0}
+  (,) w b -> f32_gt (f32_add (f32_dot n w a) b) f32_0}-}
 
 print_training_set [] = return ()
 print_training_set ((xs,y) : t) =
@@ -49,12 +52,12 @@ kernel_predict_specialized kparams ak =
   kernel_predict_des n des (KPerceptronDes.linear_kernel n) list_Foldable kparams ak
        
 sampler hyperplane _ f =
-  do { t <- training_set hyperplane n m
-     ; putStrLn "Training Set:"
-     ; print_training_set t
-     ; f t }
+  do { --t <- training_set hyperplane n m
+     --; putStrLn "Training Set:"
+     --; print_training_set t
+      f hyperplane }
 
-training_example O = return []
+{-training_example O = return []
 training_example (S n) =
   do { r <- randomRIO (-1.0,1.0)
      ; e <- training_example n
@@ -73,7 +76,7 @@ training_set hyperplane n (S m)
        ; t <- training_set hyperplane n m
        ; return $ r : t }
 
-test_set = training_set
+       test_set = training_set-}
 
 print_generalization_err test (model, train) =
   let corrects dataset = 
@@ -91,10 +94,27 @@ print_generalization_err test (model, train) =
        ++ "Test: " ++ show percent_correct_test ++ "\n"
        ++ "Generalization Error: "
        ++ show (abs (percent_correct_training - percent_correct_test))
+       
+format_line :: [[Char]] -> [Float32]
+format_line [] = []
+format_line (x : t) = 
+    [(read x :: Float32)] ++ (format_line t)
+
+format_label :: [Char] -> Bool
+format_label y = 
+    if y == "True" then True else False
+
+format_lines :: [[Char]] -> [([Float32],Bool)]
+format_lines [] = []
+format_lines (x : t) = 
+    let y = (splitOn "," x) in
+        [(format_line (init (tail y)), format_label (last y))] ++ (format_lines t)
      
 main = 
-  do { hyperplane <- training_example n
-     ; test <- test_set hyperplane n m
-     ; kperceptrondes n des epochs 20.0 (KPerceptronDes.linear_kernel n) {-(budget_update n sv list_Foldable)-} list_Foldable (sampler hyperplane) dist
+  do { train_file <- readFile "data/out1train.dat";
+        test_file <- readFile "data/out1test.dat";
+        let train = format_lines (lines train_file) in
+        let test = format_lines (lines test_file) in
+     kperceptrondes n des epochs 100.0 (KPerceptronDes.linear_kernel n) list_Foldable (sampler train) dist
      makeDesParams (print_generalization_err test) }
          
